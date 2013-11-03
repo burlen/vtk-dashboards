0359db3da311e77df4c14617f507123008f17487
Burlen Loring <bloring@lbl.gov>
2013-11-03 15:44:18 -0800

    rocky: fixed bad paths after mv

--- osmesa-llvmpipe.sh
+++ osmesa-llvmpipe.sh
@@ -3,4 +3,4 @@ export DISPLAY=:0.0
 export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib:$LD_LIBRRY_PATH
 export PATH=//usr/lib64/openmpi/bin:$PATH
 export LP_NUM_THREADS=8
-ctest -S /work/dashboards/vtk/osmesa-llvmpipe/osmesa-llvmpipe.cmake --parallel 2 --timeout 120 --output-on-failure -O osmesa-llvmpipe.log -V
+ctest -S /work/dashboards/vtk/osmesa-llvmpipe.cmake --parallel 2 --timeout 120 --output-on-failure -O osmesa-llvmpipe.log -V
